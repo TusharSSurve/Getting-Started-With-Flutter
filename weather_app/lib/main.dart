4d66d8287fb3a9a729206f3b44f8e41bf1ed6358
Tushar Surve <47970290+ttusharsurve567@users.noreply.github.com>
2021-04-16 08:25:40 +0530

    Weather App

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red.shade600,
+      body: Weather(),
+    );
+  }
+}
---
+++ weather_app/lib/weather.dart
@@ -0,0 +1,135 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+
+class Weather extends StatefulWidget {
+  @override
+  _WeatherState createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  var temp, description, currently, humidity, windSpeed, cityName;
+
+  Future<Position> getPosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+        forceAndroidLocationManager: true);
+    print('Hello');
+    return position;
+  }
+
+  Future getWeather(Position position) async {
+    print('Hello');
+    http.Response response = await http.get(Uri.parse(
+        "http://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&units=metric&appid=101216bedbd5c6f8cd832513a72fac63"));
+
+    var results = jsonDecode(response.body);
+
+    setState(() {
+      if (response.statusCode == 200) {
+        this.temp = results['main']['temp'];
+        this.description = results['weather'][0]['description'];
+        this.currently = results['weather'][0]['main'];
+        this.humidity = results['main']['humidity'];
+        this.windSpeed = results['wind']['speed'];
+        this.cityName = results['name'];
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getPosition().then((position) {
+      this.getWeather(position);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 3,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.red.shade600,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Text(
+                  'Currently in $cityName',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Text(
+                temp != null ? temp.toString() + '\u00B0' : 'Loading..',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Text(
+                  currently != null ? currently.toString() : 'Loading',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600),
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(
+            height: MediaQuery.of(context).size.height * 2 / 3,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)),
+                color: Colors.white),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                    title: Text('Temperature'),
+                    trailing: Text(temp != null
+                        ? temp.toString() + '\u00B0'
+                        : 'Loading..'),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text('Weather'),
+                    trailing: Text(description != null
+                        ? description.toString()
+                        : 'Loading..'),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text('Humidity'),
+                    trailing: Text(
+                        humidity != null ? humidity.toString() : 'Loading..'),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text('Wind Speed'),
+                    trailing: Text(
+                        windSpeed != null ? windSpeed.toString() : 'Loading..'),
+                  ),
+                ],
+              ),
+            )),
+      ],
+    );
+  }
+}
