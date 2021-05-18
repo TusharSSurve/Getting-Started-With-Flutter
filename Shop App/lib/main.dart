272e42a98e1ae9f1ecfd9a981c7193d42d2d0345
Tushar Surve <47970290+ttusharsurve567@users.noreply.github.com>
2021-05-18 13:09:14 +0530

    added edit product screen

--- Shop App/lib/main.dart
+++ Shop App/lib/main.dart
@@ -3,10 +3,12 @@ import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detailed_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screens/user_products_screen.dart';
 
 void main() {
   runApp(MultiProvider(
@@ -40,7 +42,9 @@ class MyApp extends StatelessWidget {
         ProductOverviewScreen.routeName: (ctx) => ProductOverviewScreen(),
         ProductDetailedScreen.routeName: (ctx) => ProductDetailedScreen(),
         CartScreen.routeName: (ctx) => CartScreen(),
-        OrdersScreen.routeName: (ctx) => OrdersScreen()
+        OrdersScreen.routeName: (ctx) => OrdersScreen(),
+        UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+        EditProductScreen.routeName: (ctx) => EditProductScreen()
       },
     );
   }
--- Shop App/lib/screens/user_products_screen.dart
+++ Shop App/lib/screens/user_products_screen.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
+import 'package:shop_app/widgets/user_product_item.dart';
 
 class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user_products';
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Products'),
-        actions: [IconButton(icon: Icon(Icons.add), onPressed: () {})],
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+          )
+        ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-          itemBuilder: (ctx, index) {},
+          itemBuilder: (ctx, index) {
+            return Column(
+              children: [
+                UserProductItem(
+                  title: productsData.items[index].title,
+                  imageUrl: productsData.items[index].imageUrl,
+                ),
+                Divider()
+              ],
+            );
+          },
+          itemCount: productsData.items.length,
         ),
       ),
     );
