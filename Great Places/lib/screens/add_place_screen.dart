b7d0ac58f24f97c23c3f302d26b5c4935f6d238b
Tushar Surve <47970290+ttusharsurve567@users.noreply.github.com>
2021-05-26 20:07:55 +0530

    pick image

---
+++ Great Places/lib/screens/add_place_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a New Place'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: _titleController,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ImageInput()
+                ],
+              ),
+            ),
+          )),
+          RaisedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+            label: Text('Add Place'),
+            elevation: 0,
+            color: Theme.of(context).accentColor,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ Great Places/lib/screens/places_list_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/screens/add_place_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+              })
+        ],
+      ),
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
